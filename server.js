cdb82fde7fb08caf558486063a14c9d10a0667b4
sinwailam193 <sinwailam193@hotmail.com>
2016-06-10 20:14:34 -0700

    Added procfile

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const PORT = 3000;
+const app = express();
+
+app.listen(PORT);
+app.use(express.static(__dirname + '/public'));
+console.log('listening on port: ' + PORT);
