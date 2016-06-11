25125b2a081485b421248d6a64934570d9a6a9e2
sinwailam193 <sinwailam193@hotmail.com>
2016-06-10 19:33:35 -0700

    created a web version

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
-# electron-quick-start
+# electron-react-redux
 
 Building a music desktop app using Electron and React
+
+If you do not want to download electron, this is the web version
+
+Simple:
+1. npm install
+2. npm start
---
+++ server.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const port = process.env.PORT || 8080;;
+const app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.listen(port);
+
+console.log("listening on " + port);
